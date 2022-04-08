0b6c9ecd286e9862731fc9086802addcd1af1cee
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-04-08 11:59:16 +0900

    [ADD] 상단 UserInformation 부분 레이아웃 완료(#1)

---
+++ AppleStop/AppleStop/Screens/Character/Component/UserInformationView.swift
@@ -0,0 +1,23 @@
+//
+//  UserInformationView.swift
+//  AppleStop
+//
+//  Created by SHIN YOON AH on 2022/04/08.
+//
+
+import SwiftUI
+
+struct UserInfomationView: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 138)
+            .cornerRadius(12)
+            .shadow(color: .gray, radius: 6, x: 0, y: 2)
+    }
+}
+
+struct UserInfomationView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserInfomationView()
+    }
+}
--- AppleStop/AppleStop/Screens/Character/View/CharacterView.swift
+++ AppleStop/AppleStop/Screens/Character/View/CharacterView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct CharacterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            UserInfomationView()
+                .padding(.horizontal, 24)
+        }
+            .navigationTitle("캐릭터 보관함")
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
 
