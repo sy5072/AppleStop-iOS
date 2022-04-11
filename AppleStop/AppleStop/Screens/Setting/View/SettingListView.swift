681907090a8eb8bb2f531ff91001b839c91979c3
Nick(Seungyun Kim) <sy5072@naver.com>
2022-04-11 20:39:50 +0900

    Refactoring SettingView

---
+++ AppleStop/AppleStop/Screens/Setting/View/SettingListView.swift
@@ -0,0 +1,95 @@
+//
+//  SettingListView.swift
+//  AppleStop
+//
+//  Created by Seungyun Kim on 2022/04/11.
+//
+
+import SwiftUI
+
+struct SettingListView: View {
+    
+    let title : String
+    let navIndex : Int
+    @State var hasAlarm : Bool
+    @State var isAlarm: Bool
+    
+    var body: some View {
+
+        ZStack{
+            Rectangle()
+                .fill(.white)
+                .frame(width: 320, height: 45)
+                .cornerRadius(10)
+                .shadow(color: .gray, radius: 5, x: 2, y: 2)
+            
+            if hasAlarm {
+                HStack{
+                    Text("알림 설정")
+                    Spacer()
+                    Toggle("notification", isOn: $isAlarm)
+                        .labelsHidden()
+                        .frame(height: 20)
+                }
+                .padding(12)
+                .padding(.horizontal, 10)
+              
+            } else {
+                if navIndex==2 {
+                    NavigationLink(destination: OnboardingViewTwo() ){
+                        HStack{
+                            Text("\(title)")
+                            Spacer()
+                        }
+                        .padding(12)
+                    }
+                    .padding(.horizontal, 10)
+                } else if navIndex == 3 {
+                    NavigationLink(destination: FeedbackView() ){
+                        HStack{
+                            Text("\(title)")
+                            Spacer()
+                        }
+                        .padding(12)
+                    }
+                    .padding(.horizontal, 10)
+                } else if navIndex == 4 {
+                    NavigationLink(destination: PrivacyPolicyView() ){
+                        HStack{
+                            Text("\(title)")
+                            Spacer()
+                        }
+                        .padding(12)
+                    }
+                    .padding(.horizontal, 10)
+                } else if navIndex == 5 {
+                    NavigationLink(destination: ServiceTermView() ){
+                        HStack{
+                            Text("\(title)")
+                            Spacer()
+                        }
+                        .padding(12)
+                    }
+                    .padding(.horizontal, 10)
+                } else if navIndex == 6 {
+                    NavigationLink(destination: OnboardingViewOne() ){
+                        HStack{
+                            Text("\(title)")
+                            Spacer()
+                        }
+                        .padding(12)
+                    }
+                    .padding(.horizontal, 10)
+                }
+            }
+        }
+        .listRowBackground(Color.clear)
+        .listRowSeparator(.hidden)
+    }
+}
+
+struct SettingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingListView(title: "hiiiii", navIndex: 1, hasAlarm: false, isAlarm: true)
+    }
+}
--- AppleStop/AppleStop/Screens/Setting/View/SettingView.swift
+++ AppleStop/AppleStop/Screens/Setting/View/SettingView.swift
@@ -17,142 +17,16 @@ struct SettingView: View {
                 .ignoresSafeArea()
             
             VStack{
-                ZStack{
-                    
-                    HStack{
-                        Image(systemName: "multiply")
-                            .padding()
-                        
-                        Spacer()
-                    }
-                    
-                    Text("설정")
-                        .font(.system(size: 16))
-                   
-                }
+                
                 
                 NavigationView {
                     List{
-                        ZStack{
-                            Rectangle()
-                                .fill(.white)
-                                .frame(width: 320, height: 45)
-                                .cornerRadius(10)
-                                .shadow(color: .gray, radius: 5, x: 2, y: 2)
-                            
-                            HStack{
-                                Text("알림 설정")
-                                Spacer()
-                                Toggle("notification", isOn: $isNotificated)
-                                    .labelsHidden()
-                                    .frame(height: 20)
-                            }
-                            .padding(12)
-                            .padding(.horizontal, 10)
-                        }
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                        
-                        ZStack{
-                            Rectangle()
-                                .fill(.white)
-                                .frame(width: 320, height: 45)
-                                .cornerRadius(10)
-                                .shadow(color: .gray, radius: 5, x: 2, y: 2)
-                            
-                            NavigationLink(destination: OnboardingViewOne()){
-                                HStack{
-                                    Text("닉네임 변경")
-                                    Spacer()
-                                }
-                                .padding(12)
-                            }
-                            .padding(.horizontal, 10)
-                        }
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                        
-                        
-                        ZStack{
-                            Rectangle()
-                                .fill(.white)
-                                .frame(width: 320, height: 45)
-                                .cornerRadius(10)
-                                .shadow(color: .gray, radius: 5, x: 2, y: 2)
-                            
-                            NavigationLink(destination: FeedbackView()){
-                                HStack{
-                                    Text("피드백 보내기")
-                                    Spacer()
-                                }
-                                .padding(12)
-                            }
-                            .padding(.horizontal, 10)
-                        }
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                        
-                        ZStack{
-                            Rectangle()
-                                .fill(.white)
-                                .frame(width: 320, height: 45)
-                                .cornerRadius(10)
-                                .shadow(color: .gray, radius: 5, x: 2, y: 2)
-                            
-                            NavigationLink(destination: PrivacyPolicyView()){
-                                HStack{
-                                    Text("개인정보 처리방침")
-                                    Spacer()
-                                }
-                                .padding(12)
-                            }
-                            .padding(.horizontal, 10)
-                        }
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                        
-                        ZStack{
-                            Rectangle()
-                                .fill(.white)
-                                .frame(width: 320, height: 45)
-                                .cornerRadius(10)
-                                .shadow(color: .gray, radius: 5, x: 2, y: 2)
-                            
-                            NavigationLink(destination: ServiceTermView()){
-                                HStack{
-                                    Text("서비스 이용약관")
-                                    Spacer()
-                                }
-                                .padding(12)
-                            }
-                            .padding(.horizontal, 10)
-                        }
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                        
-                        ZStack{
-                            Rectangle()
-                                .fill(.white)
-                                .frame(width: 320, height: 45)
-                                .cornerRadius(10)
-                                .shadow(color: .gray, radius: 5, x: 2, y: 2)
-                            
-                            NavigationLink(destination: GuideView()){
-                                HStack{
-                                    Text("수거딱대 가이드라인")
-                                    Spacer()
-                                }
-                                .padding(12)
-                            }
-                            .padding(.horizontal, 10)
-                        }
-                        .listRowBackground(Color.clear)
-                        .listRowSeparator(.hidden)
-                        
-    //                    ForEach (settings) { setting in
-    //                        cardView(setting: setting)
-    //                    }
-    //                    .listRowBackground(.clear)
+                        SettingListView(title: "알림 설정", navIndex: 1, hasAlarm: true, isAlarm: isNotificated)
+                        SettingListView(title: "닉네임 변경", navIndex: 2, hasAlarm: false, isAlarm: false)
+                        SettingListView(title: "피드백 보내기", navIndex: 3, hasAlarm: false, isAlarm: false)
+                        SettingListView(title: "개인정보 처리방침", navIndex: 4, hasAlarm: false, isAlarm: false)
+                        SettingListView(title: "서비스 이용약관", navIndex: 5, hasAlarm: false, isAlarm: false)
+                        SettingListView(title: "수거딱대 가이드라인", navIndex: 6,hasAlarm: false, isAlarm: false)
                     
                     }
                     .navigationBarTitle("")
@@ -171,41 +45,6 @@ struct SettingView: View {
         
     }
 }
-
-struct cardView: View {
-    
-    var setting: Setting
-    
-    var body : some View {
-//        NavigationLink(destination:setting.nav){
-            HStack{
-                Text(setting.title)
-            }
-            .padding()
-            .background(Color.black)
-            .foregroundColor(.white)
-            .cornerRadius(15)
-            .frame(width: 310, alignment: .leading)
-//        }
-       
-        
-    }
-    
-}
-
-struct Setting: Identifiable {
-    let id = UUID()
-    let title : String
-    let nav : String
-}
-
-var settings = [
-    Setting(title: "닉네임 변경", nav: "NicknameView"),
-    Setting(title: "피드백 보내기", nav: "FeedbackView"),
-    Setting(title: "개인정보 처리방침", nav: "RuleView"),
-    Setting(title: "서비스 이용약관", nav: "ServiceInfoView"),
-    Setting(title: "수거딱대 가이드라인", nav: "GuideView")
-]
 
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
