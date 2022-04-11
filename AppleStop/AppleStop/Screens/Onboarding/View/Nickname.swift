f3a78baf889441656cc43b574cbbb72e6808055f
Seik Oh <seikoh@naver.com>
2022-04-11 15:59:27 +0900

    [ADD] textfield 추가

--- AppleStop/AppleStop/Screens/Onboarding/View/Nickname.swift
+++ AppleStop/AppleStop/Screens/Onboarding/View/Nickname.swift
@@ -14,20 +14,46 @@ extension UIScreen{
 }
 
 struct Nickname: View {
+    
+    @State var name: String = ""
+    
     var body: some View {
         NavigationView{
             ZStack{
                 VStack{
-                    HStack{
-                        Text("수거딱대에서 사용할 닉네임을 입력해주세요.")
-                            .foregroundColor(Color.gray)
-                        Spacer()
+                    VStack{
+                        HStack{
+                            Text("수거딱대에서 사용할 닉네임을 입력해주세요.")
+                                .foregroundColor(Color.gray)
+                            Spacer()
+                            
+                        }
+                        .padding()
+                        
+                        //Textfield
+                        ZStack{
+                            TextField("  Enter your nickname", text: $name)
+                                .multilineTextAlignment(.center)
+                                .background(RoundedRectangle(cornerRadius: 12).fill(Color.white).shadow(radius:2)
+                                    .frame(width: UIScreen.screenWidth * 0.9, height: UIScreen.screenHeight * 0.05,  alignment: .center)
+                                    .shadow(color: Color.black.opacity(0.1), radius: 10, x: 10, y: 10)
+                                    .shadow(color: Color.white.opacity(0.3), radius: 10, x: -5, y: -5))
+                            /*
+                             Text("\(name)")
+                             .font(.title3)
+                             .fontWeight(.bold)
+                             .background(RoundedRectangle(cornerRadius: 12).fill(Color.white).shadow(radius:2)
+                             .frame(width: UIScreen.screenWidth * 0.9, height: UIScreen.screenHeight * 0.05,  alignment: .center)
+                             .shadow(color: Color.black.opacity(0.1), radius: 10, x: 10, y: 10)
+                             .shadow(color: Color.white.opacity(0.3), radius: 10, x: -5, y: -5))
+                             .foregroundColor(Color.gray)
+                             */
+                        }
                     }
+                    
                     
                     //TODO: 글자를 입력할 수 있도록 처리
                     //TODO: 글자수 제한은 8문자로 제한
-                    RoundedRectangle(cornerRadius: 12).fill(Color.white).shadow(radius:2)
-                        .frame(width: UIScreen.screenWidth * 0.9, height: UIScreen.screenHeight * 0.05,  alignment: .center)
                     
                     //TODO: 숫자가 글자수에 따라 dynamic하게 바뀌도록 처리
                     HStack{
