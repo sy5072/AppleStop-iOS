fe40d02035392fe9e48cbad790ce0127655c3ac0
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-04-11 15:38:18 +0900

    [FEAT] Character Component 내부 구성 완료(#1)

--- AppleStop/AppleStop/Screens/Character/Component/CharacterCardView.swift
+++ AppleStop/AppleStop/Screens/Character/Component/CharacterCardView.swift
@@ -18,6 +18,28 @@ struct CharacterCardView: View {
             .frame(height: 165)
             .cornerRadius(12)
             .shadow(color: .gray.opacity(0.5), radius: 1, x: 0, y: 2)
+            .overlay {
+                innerView
+            }
+    }
+}
+
+extension CharacterCardView {
+    var innerView: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image(systemName: "checkmark.square.fill")
+                    .foregroundColor(.mainGreen)
+            }
+            .padding(.trailing, 11)
+            
+            Rectangle()
+                .frame(width: 74, height: 95, alignment: .center)
+            
+            Text("하늘다람쥐")
+                .font(.system(size: 12, weight: .light, design: .default))
+        }
     }
 }
 
