14e6b3f8fe576c4b4f61d5882191d5dd13c473d4
Nick(Seungyun Kim) <sy5072@naver.com>
2022-04-11 21:38:31 +0900

    TabBar UI 구현

--- AppleStop/AppleStop/Screens/Tabbar/View/TabbarView.swift
+++ AppleStop/AppleStop/Screens/Tabbar/View/TabbarView.swift
@@ -9,27 +9,26 @@ import SwiftUI
 
 struct TabbarView: View {
     var body: some View {
-        TabView{
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("홈")
+        GeometryReader{ geometry in
+            VStack{
+                Spacer()
+                Text("Home")
+                Spacer()
+                HStack{
+                    TabBarIcon(width: geometry.size.width/5, height: geometry.size.height/30, systemIconName: "house", tabName: "홈", isCamera: false)
+                        .shadow(radius: 0)
+                    TabBarIcon(width: geometry.size.width/4, height: geometry.size.height/4, systemIconName: "", tabName: "", isCamera: true)
+                        
+                    TabBarIcon(width: geometry.size.width/6, height: geometry.size.height/30, systemIconName: "book", tabName: "분리수거 가이드", isCamera: false)
+                        .shadow(radius: 0)
                 }
-            
-            CameraView()
-                .tabItem {
-                        Image(systemName: "camera")
-
-                }
-            
-            GuideView()
-                .tabItem {
-                    Image(systemName: "book")
-                    Text("분리수거 가이드")
-                }
+                .frame(width: geometry.size.width, height: geometry.size.height/9)
+                .background(.white)
+                .shadow(radius: 2)
+                
+            }
+            .edgesIgnoringSafeArea(.bottom)
         }
-        
-        
         
     }
 }
@@ -37,5 +36,35 @@ struct TabbarView: View {
 struct TabbarView_Previews: PreviewProvider {
     static var previews: some View {
         TabbarView()
+    }
+}
+
+struct TabBarIcon: View {
+    
+    let width, height : CGFloat
+    let systemIconName, tabName : String
+    let isCamera : Bool
+    
+    var body: some View {
+        VStack{
+            if isCamera {
+                Image("logo_barcode")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: width, height: height)
+                    .padding(.horizontal, 20)
+                    .offset(y: -height/6)
+            } else {
+                Image(systemName: systemIconName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: width, height: height)
+                    .padding(.top, 10)
+                Text(tabName)
+                    .font(.footnote)
+        
+            }
+            
+        }
     }
 }
