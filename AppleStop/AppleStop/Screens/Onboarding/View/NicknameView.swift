9d0fa01fa5870636ade0b22bc8a6a090933eede0
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-04-12 17:30:45 +0900

    [CHORE] NicknameView 수정(#41)

--- AppleStop/AppleStop/Screens/Onboarding/View/NicknameView.swift
+++ AppleStop/AppleStop/Screens/Onboarding/View/NicknameView.swift
@@ -17,62 +17,78 @@ struct NicknameView: View {
     //@State var name: String = ""
     //@AppStorage("nickname") var nickname : String = UserDefaults.standard.string(forKey: "nickname")
     
+    @Environment(\.presentationMode) private var presentationMode
     @AppStorage("nickname") var nickname : String = ""
     let textLimit = 8
+    @Environment(\.viewController) private var viewControllerHolder: UIViewController?
+    
+    private var viewController: UIViewController? {
+        self.viewControllerHolder
+    }
     
     var body: some View {
-        ZStack{
-            VStack{
-                VStack{
-                    VStack{
-                        HStack{
-                            Text("닉네임 설정")
-                                .font(.title)
-                                .fontWeight(.semibold)
-                                .multilineTextAlignment(.leading)
-                                .padding(.bottom, -3.0)
-                            Spacer()
-                        }
-                        HStack{
-                            Text("수거딱대에서 사용할 닉네임을 입력해주세요.")
-                                .foregroundColor(Color.iconGrey)
-                            Spacer()
-                        }
-                        
-                        //Textfield
-                        ZStack{
-                            TextField("", text: $nickname)
-                                .ignoresSafeArea(.keyboard) //키보드가 올라오면 알아서 설정하기 버튼이 올라갑니다
-                                .onReceive(Just(nickname)) { _ in limitText(textLimit) }
-                                .multilineTextAlignment(.center)
-                                .background(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1).frame(width: UIScreen.screenWidth * 0.92, height: UIScreen.screenHeight * 0.06,  alignment: .center))
-                            
-                        }
-                        .padding()
-                    }
-                    //글자수 나타내기
-                    HStack{
-                        Spacer()
-                        Text("\(nickname.count)/8")
-                            .foregroundColor(Color.charOrange)
-                            .padding(.top,0.0)
-                    }
+        VStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Image(systemName: "xmark")
+                            .tint(.black)
+                            .frame(width: 42, height: 42, alignment: .center)
+                    })
                 }
                 
-                //설정하기 버튼
-                Spacer()
-                NavigationLink(destination: HomeView()) {
-                    Text("설정하기")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .background(RoundedRectangle(cornerRadius: 12).fill(Color.white)
-                            .frame(width: UIScreen.screenWidth * 0.75, height: UIScreen.screenHeight * 0.06,  alignment: .center))
+                VStack(alignment: .leading){
+                    Text("닉네임 설정")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.bottom, -3.0)
+                    
+                    Text("수거딱대에서 사용할 닉네임을 입력해주세요.")
+                        .foregroundColor(Color.iconGrey)
+                }
+                
+                //Textfield
+                ZStack{
+                    TextField("", text: $nickname)
+                        .ignoresSafeArea(.keyboard) //키보드가 올라오면 알아서 설정하기 버튼이 올라갑니다
+                        .onReceive(Just(nickname)) { _ in limitText(textLimit) }
+                        .multilineTextAlignment(.center)
+                        .background(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 1).frame(width: UIScreen.screenWidth * 0.92, height: UIScreen.screenHeight * 0.06,  alignment: .center))
+                    
+                }
+                .padding()
+                
+                HStack{
+                    Spacer()
+                    Text("\(nickname.count)/8")
                         .foregroundColor(Color.charOrange)
-                        .customShadow()
+                        .padding(.top,0.0)
                 }
             }
-            .padding()
+            
+            Spacer()
+            
+            Button(action: {
+                self.viewController?.present(style: .fullScreen, transitionStyle: .coverVertical) {
+                    TabbarView(viewRouter: ViewRouter())
+                }
+            }, label: {
+                Text("설정하기")
+                    .foregroundColor(Color.charOrange)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color.white).frame(width: UIScreen.screenWidth * 0.75, height: UIScreen.screenHeight * 0.06,  alignment: .center).customShadow())
+            })
+            
+            Spacer()
+                .frame(height: 68)
         }
+        .padding()
     }
     
     //8글자로 제한
