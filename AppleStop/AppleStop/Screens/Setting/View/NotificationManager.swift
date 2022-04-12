0b58cb3b6da5bf1cd7ad5d4515828f7125f99d1c
Nick(Seungyun Kim) <sy5072@naver.com>
2022-04-12 16:05:35 +0900

    [ADD] Notification Manager

---
+++ AppleStop/AppleStop/Screens/Setting/View/NotificationManager.swift
@@ -0,0 +1,20 @@
+//
+//  Notification.swift
+//  AppleStop
+//
+//  Created by Seungyun Kim on 2022/04/12.
+//
+
+import SwiftUI
+
+struct Notification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Notification_Previews: PreviewProvider {
+    static var previews: some View {
+        Notification()
+    }
+}
